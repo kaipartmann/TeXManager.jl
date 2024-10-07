14d652fce216f3b7c89523dc16f0845383060949
Documenter.jl <documenter@juliadocs.github.io>
2024-10-07 12:20:16 +0000

    build based on 52f95e8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TeXManager","category":"page"},{"location":"#TeXManager","page":"Home","title":"TeXManager","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TeXManager.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TeXManager]","category":"page"}]
+}
